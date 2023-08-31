32a314e89b56a67b8ec88cf66db93a732a0583c2
FrancescoBoco <francesco.bocoux@gmail.com>
2023-08-31 18:16:04 +0200

    ADD bonus

--- esercizio.sql
+++ esercizio.sql
@@ -38,3 +38,8 @@ WHERE degrees.department_id = 5;
 --7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti
 --per ogni esame, stampando anche il voto massimo. Successivamente,
 --filtrare i tentativi con voto minimo 18.
+
+SELECT students.id, students.surname, students.name, COUNT(*) as 'tentativi', MAX(exam_student.vote) as 'voto massimo' FROM `students` INNER JOIN exam_student ON students.id = exam_student.student_id INNER JOIN exams ON exam_student.exam_id = exams.id GROUP BY students.id;
+
+
+SELECT students.id, students.surname, students.name, COUNT(*) as 'tentativi', MAX(exam_student.vote) as 'voto massimo' FROM `students` INNER JOIN exam_student ON students.id = exam_student.student_id INNER JOIN exams ON exam_student.exam_id = exams.id WHERE vote > 17 GROUP BY students.id;
